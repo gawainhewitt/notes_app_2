9c7411ecef10982dc9b9326ddd55281f14899a93
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-04 00:07:33 +0100

    written test and mocks for notes Api

---
+++ notesApi.test.js
@@ -0,0 +1,21 @@
+const NotesApi = require('./notesApi');
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(['This note is coming from the server']),
+  })
+);
+
+
+describe('notesApi class', () => {
+  it('calls fetch and loads note from server', async () => {
+    const api = new NotesApi();
+    let result;
+    await api.loadnotes('http://localhost:3000/notes', (notes) => {
+      result = notes;
+    });
+    expect(result[0]).toBe('This note is coming from the server');
+    })
+});
+
+
