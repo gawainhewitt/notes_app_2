5778280add31fdca1546180fe271566316cd7ca4
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-04 00:10:05 +0100

    refactor

--- notesApi.js
+++ notesApi.js
@@ -13,17 +13,3 @@ class NotesApi {
 module.exports = NotesApi;
 
 
-// class GitHubApi {  
-//   async getRepoInfo(repoName, callback) {
-//     try {
-//       const result = await fetch(
-//         `https://api.github.com/repos/${repoName}`
-//       );
-//       const data = await result.json();
-//       console.log(data);
-//       callback(data);
-//     } catch (error) {
-//       return null;
-//     }
-//   }
-// }
