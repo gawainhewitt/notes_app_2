15ddb897064feb4531d74b1e9ccab3ac23f2a734
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-04 19:03:39 +0100

    refactor

--- notesApi.js
+++ notesApi.js
@@ -8,7 +8,27 @@ class NotesApi {
       callback(null);
     }
   }
-  // createNote(note) {
+ 
+  async createNote(note) {
+    const response = await fetch('http://localhost:3000/notes', {
+      method: "POST",   // method type
+
+      // Adding headers to the request
+      
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      },
+      // Adding body or contents to send
+      body: JSON.stringify({
+          content: note 
+      })
+      
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+
+   // createNote(note) {
   //   fetch('http://localhost:3000/notes', {
   //     method: "POST",   // method type
   //     // Adding body or contents to send
@@ -28,25 +48,6 @@ class NotesApi {
   //   // Displaying results to console
   //   .then(json => console.log(json));
   // }
-
-  async createNote(note) {
-    const response = await fetch('http://localhost:3000/notes', {
-      method: "POST",   // method type
-
-      // Adding headers to the request
-      
-      headers: {
-        "Content-type": "application/json; charset=UTF-8"
-      },
-      // Adding body or contents to send
-      body: JSON.stringify({
-          content: note 
-      })
-      
-    });
-    const data = await response.json();
-    console.log(data);
-  }
 
 }
 
