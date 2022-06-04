974df12c46f33e77eefe9b9cda7562c10d3057cb
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-04 13:44:33 +0100

    insert api into model

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const NotesView = require("./notesView");
 const NotesApi = require("./notesApi");
 
 const api = new NotesApi;
-const model = new NotesModel;
+const model = new NotesModel(api);
 const view = new NotesView(model);
 
 api.loadNotes((notes) => {
