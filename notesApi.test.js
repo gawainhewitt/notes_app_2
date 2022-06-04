a056977a1e1b500652b268a6c83b671a2406f8b7
Gawain Hewitt <gawain@gawainhewitt.co.uk>
2022-06-04 13:43:41 +0100

    working tests for loadNotes

--- notesApi.test.js
+++ notesApi.test.js
@@ -1,21 +1,76 @@
 const NotesApi = require('./notesApi');
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve(['This note is coming from the server']),
-  })
-);
+require('jest-fetch-mock').enableMocks();
 
+beforeEach(() => {
+  fetch.resetMocks();
+});
 
 describe('notesApi class', () => {
   it('calls fetch and loads note from server', async () => {
     const api = new NotesApi();
+    fetch.mockResponseOnce(JSON.stringify(['This note is coming from the server']));
     let result;
     await api.loadNotes((notes) => {
       result = notes;
     });
     expect(result[0]).toBe('This note is coming from the server');
+    expect(fetch).toHaveBeenCalledWith('http://localhost:3000/notes');
+    expect(fetch).toHaveBeenCalledTimes(1);
     })
+  it('handles exception with null', async () => {
+    fetch.mockReject(() => Promise.reject('API failure'))
+    const api = new NotesApi();
+    let result;
+    await api.loadNotes((notes) => {
+      result = notes;
+    });
+    expect(result).toBe(null);
+    expect(fetch).toHaveBeenCalledWith('http://localhost:3000/notes');
+    expect(fetch).toHaveBeenCalledTimes(1);
+
+  })
 });
+  
+  
+  
+
+// manual mocks below vvvv
+
+// global.fetch = jest.fn(() =>
+//   Promise.resolve({
+//     json: () => Promise.resolve(['This note is coming from the server']),
+//   })
+// );
+
+// beforeEach(() => {
+//   fetch.mockClear();
+// });
+
+
+// describe('notesApi class', () => {
+//   it('calls fetch and loads note from server', async () => {
+//     const api = new NotesApi();
+//     let result;
+//     await api.loadNotes((notes) => {
+//       result = notes;
+//     });
+//     expect(result[0]).toBe('This note is coming from the server');
+//     expect(fetch).toHaveBeenCalledWith('http://localhost:3000/notes');
+//     expect(fetch).toHaveBeenCalledTimes(1);
+//     })
+//   it('handles exception with null', async () => {
+//     fetch.mockImplementationOnce(() => Promise.reject('API failure'))
+//     const api = new NotesApi();
+//     let result;
+//     await api.loadNotes((notes) => {
+//       result = notes;
+//     });
+//     expect(result).toBe(null);
+//     expect(fetch).toHaveBeenCalledWith('http://localhost:3000/notes');
+//     expect(fetch).toHaveBeenCalledTimes(1);
+
+//   })
+// });
 
 
